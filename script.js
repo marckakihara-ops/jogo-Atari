1b05a60b69ec448986db14d1ec26130b7296c047
marckakihara-ops <marckakihara@hotmail.com>
2026-05-28 15:28:23 -0300

    My first upload

---
+++ script.js
@@ -0,0 +1,164 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 500;
+
+const player = {
+    x: 10,
+    y: 200,
+    width: 15,
+    height: 100,
+    speed: 8
+};
+
+const cpu = {
+    x: 775,
+    y: 200,
+    width: 15,
+    height: 100,
+    speed: 4
+};
+
+const ball = {
+    x: 400,
+    y: 250,
+    radius: 10,
+    speedX: 5,
+    speedY: 5
+};
+
+let upPressed = false;
+let downPressed = false;
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowUp") {
+        upPressed = true;
+    }
+
+    if (event.key === "ArrowDown") {
+        downPressed = true;
+    }
+});
+
+document.addEventListener("keyup", (event) => {
+    if (event.key === "ArrowUp") {
+        upPressed = false;
+    }
+
+    if (event.key === "ArrowDown") {
+        downPressed = false;
+    }
+});
+
+function drawPlayer() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(
+        player.x,
+        player.y,
+        player.width,
+        player.height
+    );
+}
+
+function drawCPU() {
+    ctx.fillRect(
+        cpu.x,
+        cpu.y,
+        cpu.width,
+        cpu.height
+    );
+}
+
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(
+        ball.x,
+        ball.y,
+        ball.radius,
+        0,
+        Math.PI * 2
+    );
+
+    ctx.fill();
+    ctx.closePath();
+}
+
+function movePlayer() {
+    if (upPressed && player.y > 0) {
+        player.y -= player.speed;
+    }
+
+    if (
+        downPressed &&
+        player.y < canvas.height - player.height
+    ) {
+        player.y += player.speed;
+    }
+}
+
+function moveCPU() {
+    if (cpu.y + cpu.height / 2 < ball.y) {
+        cpu.y += cpu.speed;
+    }
+
+    if (cpu.y + cpu.height / 2 > ball.y) {
+        cpu.y -= cpu.speed;
+    }
+}
+
+function moveBall() {
+    ball.x += ball.speedX;
+    ball.y += ball.speedY;
+
+
+    if (
+        ball.y + ball.radius > canvas.height ||
+        ball.y - ball.radius < 0
+    ) {
+        ball.speedY *= -1;
+    }
+
+    if (
+        ball.x - ball.radius < player.x + player.width &&
+        ball.y > player.y &&
+        ball.y < player.y + player.height
+    ) {
+        ball.speedX *= -1;
+    }
+
+    if (
+        ball.x + ball.radius > cpu.x &&
+        ball.y > cpu.y &&
+        ball.y < cpu.y + cpu.height
+    ) {
+        ball.speedX *= -1;
+    }
+
+    if (ball.x < 0 || ball.x > canvas.width) {
+        ball.x = canvas.width / 2;
+        ball.y = canvas.height / 2;
+    }
+}
+
+function update() {
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    movePlayer();
+    moveCPU();
+    moveBall();
+
+    drawPlayer();
+    drawCPU();
+    drawBall();
+
+    requestAnimationFrame(update);
+}
+
+update();
